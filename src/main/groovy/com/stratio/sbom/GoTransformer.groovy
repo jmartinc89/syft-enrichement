72ffb69c864ca97d231883ee3dc53dc31dfcfc8f
jmartinc89 <jorgemcsg89@gmail.com>
2023-07-13 11:58:20 +0200

    make productive

--- src/main/groovy/com/stratio/sbom/GoTransformer.groovy
+++ src/main/groovy/com/stratio/sbom/GoTransformer.groovy
@@ -7,12 +7,15 @@ class GoTransformer {
     static void main(String[] args) {
         // Run Syft to generate SBOM to be tampered
         def sbom = new Sbom(args[0])
-
+        new File("${args[0].split("/")[-1]}-orig.json").withWriter('utf8') { writer ->
+            writer.write(sbom.toString())
+        }
         // SBOM is preserved if main artifact was developed in any language but golang
         def mainArtifact = sbom.getMainBinaryArtifact()
         if (mainArtifact != null && mainArtifact.language == "go") {
             // Extract main attributes of the main artifact
-            def compilerVersion = mainArtifact["metadata"]["goCompiledVersion"]
+            def compilerVersion =
+                    mainArtifact["metadata"]["goCompiledVersion"].toString().replaceAll("go", "")
             def locationPath = mainArtifact["locations"][0]["path"]
             def locationLayerID = mainArtifact["locations"][0]["layerID"]
 
@@ -33,6 +36,8 @@ class GoTransformer {
             sbom.addNewArtifactRelationship(builderSourceRelation)
             sbom.addNewArtifactRelationship(builderLocationRelation)
         }
-        println sbom.toString()
+        new File("${args[0].split("/")[-1]}-mod.json").withWriter('utf8') { writer ->
+            writer.write(sbom.toString())
+        }
     }
 }
